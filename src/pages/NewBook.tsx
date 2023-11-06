de51f0fca9c107ce27d8b101fd02e5d7c714a12e
Craig Ambrose <craig@craigambrose.com>
2023-11-06 17:51:42 +1100

    add example form

--- src/pages/NewBook.tsx
+++ src/pages/NewBook.tsx
@@ -1,9 +1,55 @@
-import { Container } from "@chakra-ui/react"
+import { useForm } from "react-hook-form"
+import {
+  FormErrorMessage,
+  FormLabel,
+  FormControl,
+  Input,
+  Button,
+  Container,
+} from "@chakra-ui/react"
 
 export default function NewBook() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm()
+
+  function onSubmit(values) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        alert(JSON.stringify(values, null, 2))
+        resolve()
+      }, 3000)
+    })
+  }
+
   return (
     <Container padding={4}>
-      Add book
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormControl isInvalid={errors.name}>
+          <FormLabel htmlFor="name">First name</FormLabel>
+          <Input
+            id="name"
+            placeholder="name"
+            {...register("name", {
+              required: "This is required",
+              minLength: { value: 4, message: "Minimum length should be 4" },
+            })}
+          />
+          <FormErrorMessage>
+            {errors.name && errors.name.message}
+          </FormErrorMessage>
+        </FormControl>
+        <Button
+          mt={4}
+          colorScheme="teal"
+          isLoading={isSubmitting}
+          type="submit"
+        >
+          Submit
+        </Button>
+      </form>
     </Container>
   )
 }
