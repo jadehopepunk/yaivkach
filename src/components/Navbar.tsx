bb6a1627b12f29d9e57198fc7bc7443e87250153
Craig Ambrose <craig@craigambrose.com>
2023-11-06 16:42:32 +1100

    package upgrades

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,14 @@
 "use client"
 
 import { Box, Flex, useColorModeValue, Stack } from "@chakra-ui/react"
-import ColourModeSwitcher from "../ColorModeSwitcher.tsx"
+import ColourModeSwitcher from "./ColorModeSwitcher.tsx"
 
 export default function Navbar() {
   return (
     <>
       <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
         <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
-          <Box>Panga Carsharing</Box>
+          <Box>Panga Libraries</Box>
 
           <Flex alignItems={"center"}>
             <Stack direction={"row"} spacing={7}>
