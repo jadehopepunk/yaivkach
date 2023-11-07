5c02e6fee49f71be342f3843c2fd29ca8ab33107
Craig Ambrose <craig@craigambrose.com>
2023-11-07 18:19:49 +1100

    can publish a book to the node

--- src/data/schemas.ts
+++ src/data/schemas.ts
@@ -1,3 +1,6 @@
 // Identifiers of the schemas we use
 export const BOOK_SCHEMA_ID =
-  "book_002068630c898d13851d24f99f2671d2a5170e8fe5dbccc5da8415884a4bcc0622aa"
+  "book_0020f66d54e5c6b7ad6399ffe64f3f62c23ba487711cc60dd4d52eca232b5a45383a"
+
+export const BOOK_CREATOR_SCHEMA_ID =
+  "book_creator_002045e4b065583d61ba64b19c8d36d43ca93d9f002d13d83d15f3b5c5c6880ddec5"
