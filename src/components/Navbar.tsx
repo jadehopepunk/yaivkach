03116d1a0667eb4d6320aa1139f486bc23e8a9f5
Craig Ambrose <craig@craigambrose.com>
2023-11-14 19:54:41 +1100

    added some ugly navbar actions

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,29 @@
 "use client"
 
-import { Box, Flex, useColorModeValue, Stack } from "@chakra-ui/react"
+import { Box, Flex, useColorModeValue, Stack, Button } from "@chakra-ui/react"
 import ColourModeSwitcher from "./ColorModeSwitcher.tsx"
+import { Link } from "react-router-dom"
 
 export default function Navbar() {
   return (
     <>
       <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
-        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
-          <Box>Panga Libraries</Box>
+        <Flex alignItems={"center"} justifyContent={"space-between"}>
+          <Stack direction="row" alignItems="center">
+            <Box>Panga Libraries</Box>
+            <Box>
+              <Link to="/books">
+                <Button colorScheme="blue" variant="ghost">
+                  Books
+                </Button>
+              </Link>
+              <Link to="/libraries">
+                <Button colorScheme="blue" variant="ghost">
+                  Libraries
+                </Button>
+              </Link>
+            </Box>
+          </Stack>
 
           <Flex alignItems={"center"}>
             <Stack direction={"row"} spacing={7}>
