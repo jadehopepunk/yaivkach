7635ed492abf123e06ba174559a94faa622f1a19
Craig Ambrose <craig@craigambrose.com>
2023-11-19 19:28:50 +1100

    Form validation for library form

--- src/data/document_types.ts
+++ src/data/document_types.ts
@@ -14,5 +14,5 @@ export type Book = {
 export type Library = {
   name: string
   short_description: string
-  long_description: string
+  long_description?: string
 }
--- src/pages/libraries/components/LibraryForm.tsx
+++ src/pages/libraries/components/LibraryForm.tsx
@@ -7,9 +7,12 @@ import {
   Input,
   Textarea,
   Button,
+  FormErrorMessage,
 } from "@chakra-ui/react"
 import { Library } from "../../../data/document_types"
 import { Doc } from "../../../p2panda-apollo"
+import { object, string } from "yup"
+import { yupResolver } from "@hookform/resolvers/yup"
 
 type LibraryFormFieldValues = FieldValues & Library
 
@@ -22,8 +25,15 @@ export default function LibraryForm({
   onSubmit,
   initialLibrary,
 }: LibraryFormProps) {
+  const validationSchema = object().shape({
+    name: string().required("Name is required"),
+    short_description: string().required("Short description is required"),
+    long_description: string(),
+  })
+
   const methods = useForm<LibraryFormFieldValues>({
     values: initialLibrary?.fields,
+    resolver: yupResolver(validationSchema),
   })
   const {
     handleSubmit,
@@ -36,17 +46,24 @@ export default function LibraryForm({
     <FormProvider {...methods}>
       <form onSubmit={handleSubmit(onSubmit)}>
         <Stack spacing={4} align="stretch">
-          <FormControl>
+          <FormControl isInvalid={!!errors.name}>
             <FormLabel>Name</FormLabel>
             <Input {...register("name")} />
+            <FormErrorMessage>{errors.name?.message}</FormErrorMessage>
           </FormControl>
-          <FormControl>
+          <FormControl isInvalid={!!errors.short_description}>
             <FormLabel>Short description</FormLabel>
             <Input {...register("short_description")} />
+            <FormErrorMessage>
+              {errors.short_description?.message}
+            </FormErrorMessage>
           </FormControl>
-          <FormControl>
+          <FormControl isInvalid={!!errors.long_description}>
             <FormLabel>Long description</FormLabel>
             <Textarea {...register("long_description")} />
+            <FormErrorMessage>
+              {errors.long_description?.message}
+            </FormErrorMessage>
           </FormControl>
           <Box>
             <Button colorScheme="teal" isLoading={isSubmitting} type="submit">
